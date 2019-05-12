912233864ce82c47b628f0c36e06119d1a5c4501
andrewdieken <dieke102@mail.chapman.edu>
2019-05-11 18:36:13 -0700

    Add the note resource to our urls file

--- noteable_django/urls.py
+++ noteable_django/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from api.resources import NoteResource
+
+note_resource = NoteResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include(note_resource.urls))
 ]
